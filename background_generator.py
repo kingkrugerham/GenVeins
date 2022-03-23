dfd8a6a1a5d9df537d845d721452d170f648eea9
kingkrugerham <16461398@sun.ac.za>
2022-03-23 09:55:49 +0200

    some more documentation and cleanup

--- background_generator.py
+++ background_generator.py
@@ -11,20 +11,39 @@ matplotlib.use('Agg')
 
 
 def make_greyscale(original_im):
+	"""
+	Function to facilitate the initial greyification of a binary hand vein image.
+	The proposed algorithm is outlined as follows:
+		- Initialize a black image of same shape as input.
+		- Loop through original image and determine whether current pixel is a vein or background pixel.
+		- Randomly choose greyscale vein or background intensity for current pixel from acceptable intensity ranges.
+		- Add minor variation to choice.
+		- Set corresponding pixel in new image equal to newly obtained intensity.
+	:param original_im: Thinned version of a given binary hand vein image.
+	:return: Initially greyified version of original_im.
+	"""
 	new_im = initialize_im()
 	for i in range(original_im.shape[0]):
 		for j in range(original_im.shape[1]):
-			vein_intensity = np.random.uniform(0.15, 0.4)
-			background_intensity = np.random.uniform(0.05, 0.14)
 			var = np.random.uniform(-0.03, 0.03)
 			if original_im[i, j] > 0:
+				vein_intensity = np.random.uniform(0.15, 0.4)
 				new_im[i, j] = vein_intensity + var
 			else:
+				background_intensity = np.random.uniform(0.05, 0.14)
 				new_im[i, j] = background_intensity + var
 	return new_im
 
 
 def normalize_vein_intensity(img):
+	"""
+	Function to normalize the vein intensity in order to avoid discontinuities introduced by initial greyification.
+	The proposed algorithm is outlined as follows.
+		- Find the average intensity of the hand vein pixels in the initially greyified image.
+		- Set each vein pixel equal to the average intensity and add minor variation to avoid a monotone vein intensity.
+	:param img: Initially greyified vein image (output of make_greyscale(original_im))
+	:return: Normalized grey scale hand vein image.
+	"""
 	new_im = initialize_im()
 	avg_vein_intensity = np.mean([img[x, y] for x in range(img.shape[0]) for y in range(img.shape[1]) if img[x, y] >= 0.18])
 	for i in range(img.shape[0]):
@@ -42,7 +61,7 @@ def main_function(root_output_dir, struct_veins, ind):
 	Orchestrator function for creating a grey-scale version of the simulated acquired hand vein-like images.
 	The proposed algorithm is outlined as follows:
 		- First apply minor erosion of the hand vein image, since subsequent smoothing will thicken vein structures.
-		- Randomly greyify vein and background pixels, selecting from an acceptable intensity range.
+		- Randomly greyify vein and background pixels by selecting from an acceptable intensity range.
 		- Normalize the vein intensities to reduce discontinuity.
 		- Apply successive smoothing with local mean and median filters.
 		- Apply intermittent grey-scale erosion in order to avoid blob-like structures.
@@ -51,11 +70,10 @@ def main_function(root_output_dir, struct_veins, ind):
 	:param ind: Numbered individual.
 	:return: None
 	"""
-
 	for f_vein in struct_veins:
-		thinned = binary_erosion(f_vein, disk(1))
-		grey = make_greyscale(thinned)
-		img = normalize_vein_intensity(grey)
+		img = binary_erosion(f_vein, disk(1))
+		img = make_greyscale(img)
+		img = normalize_vein_intensity(img)
 		img = rank.mean(img, selem=disk(3))
 		img = median(img, disk(3))
 		img = rank.mean(img, selem=disk(2))
--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from base_generator import main_function as base_main
 
 
 def main_function(root_output_dir, num_inds):
+	"""
+	Orchestrator function for the repo. The proposed algorithm is briefly outlined below, while an in depth
+	explanation of each step is documented in the corresponding module and functions.
+		- First, draw the base veins on a black image. This constitutes seed veins and branch veins.
+		- Second, create 4 copies of the base veins, adding tree veins and unconnected veins.
+		- Lastly, greyify each of the 4 copies.
+	"""
 	for ind in range(num_inds):
 		base_veins = base_main(root_output_dir, ind)
 		struct_veins = struct_main(root_output_dir, base_veins, ind)
 		back_main(root_output_dir, struct_veins, ind)
 
+
 if __name__ == "__main__":
 	root_output_dir = 'D:/PhD_Files/Input/GenVeins/'
-	num_inds = sys.argv[1]
+	num_inds = sys.argv[1]  # number of individuals in the database to be generated.
 	main_function(root_output_dir, num_inds)
 	
